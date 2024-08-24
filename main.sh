35cad27a33f191d102a38b1c3ff46a73201f4e84
akashsinghtkd <63353016+akashsinghtkd@users.noreply.github.com>
2024-08-24 10:20:36 +0530

    Create main.sh

---
+++ main.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+git filter-branch -f --env-filter '
+OLD_EMAIL="<old email>"
+CORRECT_NAME="<name>"
+CORRECT_EMAIL="<email>"
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_COMMITTER_NAME="$CORRECT_NAME"
+export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+export GIT_AUTHOR_NAME="$CORRECT_NAME"
+export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
